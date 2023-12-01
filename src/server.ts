315cd04827931ad914c1cf3f0c9dee700239f1d7
Mateus <correia.mateus@hotmail.com>
2023-11-30 22:32:23 -0300

    Server ts

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is up!");
+});
